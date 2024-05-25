9e3af626b983256a5f7969c214857bb23a0f8fe7
Damian <damia_molin@hotmail.com>
2024-05-25 14:07:28 +0200

    Update picts

--- cliente/src/assets/index.js
+++ cliente/src/assets/index.js
@@ -1,8 +1,8 @@
-import { ReactComponent as LogoWhite } from "./svg/tincode-white.svg";
+import { ReactComponent as LogoWhite } from "./svg/tincode-white.png";
 
 import authBg from "./jpg/auth-bg.jpg";
 import homeBanner from "./jpg/home-banner.jpg";
-import noAvatar from "./jpg/no-avatar.jpg";
+import noAvatar from "./jpg/no-avatar.png";
 import academyLogo from "./png/academy-logo.png";
 import avatar01 from "./png/avatar_01.png";
 import avatar02 from "./png/avatar_02.png";
